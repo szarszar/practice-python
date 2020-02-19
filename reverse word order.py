a020819287721c475cf967eb83cb328e71c0aa6f
Michał Szarpak <michal.szar@gmail.com>
2020-02-19 18:49:45 +0100

    odwracanie kolejności słów w zdaniu

---
+++ reverse word order.py
@@ -0,0 +1,14 @@
+# Write a program (using functions!) that asks the user for a long string containing multiple words.
+# Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+#
+#   My name is Michele
+#
+# Then I would see the string:
+#
+#   Michele is name My
+# shown back to me.
+
+sentance = input("Please write a message to reverse: ")
+words = sentance.split(' ')
+reversed_sentance = words[::-1]
+print(' '.join(reversed_sentance))
